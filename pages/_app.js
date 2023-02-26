6010c41472d85dbb42c7cec5af7c195ec0ce8b44
Jose <bubablue00@gmail.com>
2023-02-26 11:39:12 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
+import { Dashboard } from './Dashboard/Dashboard'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return <Dashboard/>
 }
