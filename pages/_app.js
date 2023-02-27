d5700a20340de5ec4d0b553011528b1626b0cbd7
Jose <bubablue00@gmail.com>
2023-02-27 21:31:46 +0100

    update: logout function

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
 // import '@/styles/globals.css'
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import TeamsProvider from "../Context/TeamProvider";
+import TeamsProvider, { useTeams } from "../Context/TeamProvider";
 import NhlThemeProvider from "../Context/Theme/ThemeProvider";
 import Sidebar from "../Components/Sidebar/Sidebar";
 
 export default function App({ Component, pageProps }) {
   const queryClient = new QueryClient();
+  const {logout} = useTeams()
   return (
     <QueryClientProvider client={queryClient}>
       <TeamsProvider>
         <NhlThemeProvider>
           <div>
-            <Sidebar logout={() => {}} />
+            <Sidebar logout={logout} />
             <Component {...pageProps} />
           </div>
         </NhlThemeProvider>
