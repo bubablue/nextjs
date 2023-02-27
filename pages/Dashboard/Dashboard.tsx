6ec27389cf6eb931fb04dc0a9d30c1a1c2e8aa17
Jose <bubablue00@gmail.com>
2023-02-27 09:41:21 +0100

    update styles

--- pages/Dashboard/Dashboard.tsx
+++ pages/Dashboard/Dashboard.tsx
@@ -1,49 +1,117 @@
 import React from "react";
 import { TeamLogo } from "../../Components/TeamLogo";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { makeStyles } from "@mui/styles";
-import { Theme } from "@material-ui/core";
-import { createStyles } from "@material-ui/core/styles";
+import { createStyles } from "@material-ui/core";
+import Colours from "../../Context/Theme/Colours";
+import { Theme } from "@mui/material/styles";
+import { useRouter } from "next/router";
 
 const Dashboard = () => {
   const NHL_URL = "https://statsapi.web.nhl.com/api/v1/teams";
   const [teams, setTeams] = React.useState<any>([]);
-
+const router = useRouter();
   const getTeams = async () => {
     const response = await fetch(NHL_URL);
     const data = await response.json();
     setTeams(data.teams);
-    console.log(data);
   };
 
-  const useStyles = makeStyles((_theme: Theme) => {
+
+  const useStyles = makeStyles((theme: Theme) => {
     return createStyles({
+      root: {
+        padding: "100px",
+        paddingBottom: "300px",
+        background: Colours.BW[theme.palette.mode],
+        minHeight: "100vh",
+      },
+      header: {
+        display: "flex",
+        justifyContent: "center",
+        justifySelf: "center",
+        alignItems: "center",
+        textAlign: "center",
+        background: Colours.BW_02[theme.palette.mode],
+        border: 0,
+        borderRadius: 10,
+        // boxShadow: theme.palette.mode === 'light' ? "0 3px 5px 2px grey" : 'none',
+        color: Colours.BW[theme.palette.mode],
+        height: 80,
+        padding: "10px",
+        margin: "50px 10% 50px 10%",
+        width: "80%",
+      },
+      teams: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        flexWrap: "wrap",
+        margin: "10px",
+        background: Colours.BW[theme.palette.mode],
+      },
+      team: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+        padding: "20px",
+        // border: '5px solid black',
+        borderRadius: "10px",
+        width: "200px",
+        height: "200px",
+      },
+      names: {
+        display: "block",
+        margin: "50px",
+        textDecoration: "none",
+        font: "15px Arial, sans-serif",
+        color: "grey",
+      },
       images: {
-        width: "100px",
-        height: "100px",
+        height: "200px",
+        width: "200px",
       },
     });
   });
+
   const classes = useStyles();
 
   React.useEffect(() => {
     getTeams();
   }, []);
 
+  const handleClick = () => {
+    router.push("/teams");
+  };
+
+  console.log(classes.images);
   return (
-    <div>
-      <h1></h1>
-      {teams &&
-        teams.map((team: any) => (
-          <div key={team.id}>
-            <h2>{team.name}</h2>
-            <p>{team.venue.name}</p>
-            <TeamLogo
-              classProp={classes.images}
-              team={team.name}
-              teamId={team.id}
-            />
+    <div className={classes.root}>
+      <h1 className={classes.header} onClick={handleClick}>
+        NHL TEAMS
+      </h1>
+      <div className={classes.teams}>
+        {teams?.map((team) => (
+          <div className={classes.team} key={team.shortName}>
+            {/* <a key={team.name} className={classes.names} href={team.officialSiteUrl}> */}
+            <a
+              key={team.name}
+              className={classes.names}
+              href={`/teams/${team.id}/stats`}
+            >
+              <TeamLogo
+                classProp={classes.images}
+                team={team.name}
+                teamId={team.id}
+              />
+              {team.name}
+            </a>
           </div>
         ))}
+      </div>
     </div>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,9 @@
+// This code is a custom _document.js file in Next.js.
+// It is used to set the lang attribute on the html element.
+// This is useful for accessibility and SEO.
+// The lang attribute is set to "en", which is the language of the page.
+
+
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
