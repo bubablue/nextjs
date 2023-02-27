2511e90a1a85b7444545fb563a3f9216c5624def
Jose <bubablue00@gmail.com>
2023-02-27 14:35:48 +0100

    update index

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,8 @@
 
 
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useRouter } from 'next/router';
 
 export default function Document() {
-
-  const router = useRouter();
-  router.push('/dashboard');
   return (
     <Html lang="en">
       <Head />
