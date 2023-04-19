dd986a4df90cc976bdf1bf81ef302260c1448f48
Jose <bubablue00@gmail.com>
2023-04-19 11:44:29 +0200

    update styled components

--- Components/Sidebar/UserSidebar/UserSidebar.tsx
+++ Components/Sidebar/UserSidebar/UserSidebar.tsx
@@ -11,144 +11,233 @@ import {
 } from "react-bootstrap-icons";
 import NavbarToggle from "react-bootstrap/NavbarToggle";
 // import useStyles from "./styles";
-import { createStyles } from "@material-ui/core";
-import { makeStyles } from "@mui/styles";
+// import { createStyles } from "@material-ui/core";
+// import { makeStyles } from "@mui/styles";
+import { styled } from "@mui/system";
 
-const useStyles = makeStyles((_theme) =>
-  createStyles({
-    root: {},
-    mainHeader: {
-      display: "flex",
-      justifyContent: "center",
-      justifySelf: "center",
-      alignItems: "center",
-      textAlign: "center",
-      background: "linear-gradient(45deg, rgb(0,0,0) 30%, #FF8E53 90%)",
-      border: 0,
-      borderRadius: 10,
-      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-      color: "white",
-      height: 50,
-      padding: "10px",
-      margin: "0px 5% 50px 5%",
-      width: "90%",
-    },
-    sidebarToggle: {
-      background: "transparent",
-      border: 0,
-      color: "white",
-    },
-    toggle: {
-      display: "flex",
-      justifyContent: "center",
-      justifySelf: "center",
-      alignItems: "center",
-      textAlign: "center",
-      background: "transparent",
-      border: 0,
-      color: "white",
-      height: "40px",
-      width: "40px",
-      "&:hover": {
-        color: "#FF8E53",
-        cursor: "pointer",
-      },
-    },
-    drawerOpen: {
-      position: "fixed",
-      top: 0,
-      right: "0px",
-      height: "100%",
-      background: "rgba(0,0,0,0.8)",
-      width: "200px",
-      border: "none",
-      transition: "width 0.3s ease-in-out",
-      overflowX: "hidden",
-      zIndex: 1,
-    },
-    drawerClose: {
-      position: "fixed",
-      top: 0,
-      right: "0px",
-      height: "100%",
-      background: "rgba(0,0,0,0.8)",
-      width: "70px",
-      border: "none",
-      transition: "width 0.3s ease-in-out",
-      overflowX: "hidden",
-      zIndex: 1,
-    },
-    toolbar: {
-      display: "flex",
-      alignItems: "center",
-      justifyItems: "left",
-      justifyContent: "left",
-      textAlign: "left",
-      flexDirection: "column",
-    },
-    link: {
-      textDecoration: "none",
-      textAlign: "left",
-      color: "white",
-      fontSize: "1.5rem",
-      fontWeight: "bold",
-      margin: "10px",
-      padding: "10px",
-      "&:hover": {
-        color: "#FF8E53",
-      },
-    },
-    menuItem: {},
-  })
-);
+// const useStyles = makeStyles((_theme) =>
+//   createStyles({
+//     root: {},
+//     mainHeader: {
+//       display: "flex",
+//       justifyContent: "center",
+//       justifySelf: "center",
+//       alignItems: "center",
+//       textAlign: "center",
+//       background: "linear-gradient(45deg, rgb(0,0,0) 30%, #FF8E53 90%)",
+//       border: 0,
+//       borderRadius: 10,
+//       boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+//       color: "white",
+//       height: 50,
+//       padding: "10px",
+//       margin: "0px 5% 50px 5%",
+//       width: "90%",
+//     },
+//     sidebarToggle: {
+//       background: "transparent",
+//       border: 0,
+//       color: "white",
+//     },
+//     toggle: {
+//       display: "flex",
+//       justifyContent: "center",
+//       justifySelf: "center",
+//       alignItems: "center",
+//       textAlign: "center",
+//       background: "transparent",
+//       border: 0,
+//       color: "white",
+//       height: "40px",
+//       width: "40px",
+//       "&:hover": {
+//         color: "#FF8E53",
+//         cursor: "pointer",
+//       },
+//     },
+//     drawerOpen: {
+//       position: "fixed",
+//       top: 0,
+//       right: "0px",
+//       height: "100%",
+//       background: "rgba(0,0,0,0.8)",
+//       width: "200px",
+//       border: "none",
+//       transition: "width 0.3s ease-in-out",
+//       overflowX: "hidden",
+//       zIndex: 1,
+//     },
+//     drawerClose: {
+//       position: "fixed",
+//       top: 0,
+//       right: "0px",
+//       height: "100%",
+//       background: "rgba(0,0,0,0.8)",
+//       width: "70px",
+//       border: "none",
+//       transition: "width 0.3s ease-in-out",
+//       overflowX: "hidden",
+//       zIndex: 1,
+//     },
+//     toolbar: {
+//       display: "flex",
+//       alignItems: "center",
+//       justifyItems: "left",
+//       justifyContent: "left",
+//       textAlign: "left",
+//       flexDirection: "column",
+//     },
+//     link: {
+//       textDecoration: "none",
+//       textAlign: "left",
+//       color: "white",
+//       fontSize: "1.5rem",
+//       fontWeight: "bold",
+//       margin: "10px",
+//       padding: "10px",
+//       "&:hover": {
+//         color: "#FF8E53",
+//       },
+//     },
+//     menuItem: {},
+//   })
+// );
+
+const Drawer = styled(Navbar)(({ theme }) => ({
+  position: "fixed",
+  top: 0,
+  right: 0,
+  background: "rgba(0,0,0,0.8)",
+  height: "100%",
+  border: "none",
+  transition: "width 0.3s ease-in-out",
+  overflowX: "hidden",
+  zIndex: 1,
+}));
+const NavToggle = styled(NavbarToggle)(({ theme }) => ({
+  background: "transparent",
+  border: 0,
+  color: "white",
+}));
+
+const Navigation = styled(Nav)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyItems: "left",
+  justifyContent: "left",
+  textAlign: "left",
+  flexDirection: "column",
+}));
+
+const LinkItem = styled(Nav.Link)(({ theme }) => ({
+  textDecoration: "none",
+  textAlign: "left",
+  color: "white",
+  fontSize: "1.5rem",
+  fontWeight: "bold",
+  margin: "10px",
+  padding: "10px",
+  "&:hover": {
+    color: "#FF8E53",
+  },
+}));
 
 export const UserSidebar = () => {
   const [open, setOpen] = React.useState<boolean>(false);
-  const classes = useStyles();
+  // const classes = useStyles();
   const handleDrawerOpen = () => {
     setOpen(!open);
   };
 
   return (
-    <Navbar
+    <Drawer
       bg="dark"
       variant="dark"
-      className={open ? classes.drawerOpen : classes.drawerClose}
+      style={open ? { width: "200px" } : { width: "70px" }}
     >
-      <Nav className={classes.toolbar}>
-        <NavbarToggle
-          className={classes.sidebarToggle}
-          onClick={handleDrawerOpen}
-        >
+      <Navigation>
+        <NavToggle onClick={handleDrawerOpen}>
           {open ? (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={open ? "Close Menu" : "Open Menu"}>
                   {open ? (
-                    <X className={classes.toggle} />
+                    <X
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        justifySelf: "center",
+                        alignItems: "center",
+                        textAlign: "center",
+                        background: "transparent",
+                        border: 0,
+                        color: "white",
+                        height: "40px",
+                        width: "40px",
+                      }}
+                    />
                   ) : (
-                    <List className={classes.toggle} />
+                    <List
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        justifySelf: "center",
+                        alignItems: "center",
+                        textAlign: "center",
+                        background: "transparent",
+                        border: 0,
+                        color: "white",
+                        height: "40px",
+                        width: "40px",
+                      }}
+                    />
                   )}
                 </Tooltip>
               </p>
             </div>
           ) : (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={open ? "Close Menu" : "Open Menu"}>
                   {open ? (
-                    <X className={classes.toggle} />
+                    <X
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        justifySelf: "center",
+                        alignItems: "center",
+                        textAlign: "center",
+                        background: "transparent",
+                        border: 0,
+                        color: "white",
+                        height: "40px",
+                        width: "40px",
+                      }}
+                    />
                   ) : (
-                    <List className={classes.toggle} />
+                    <List
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        justifySelf: "center",
+                        alignItems: "center",
+                        textAlign: "center",
+                        background: "transparent",
+                        border: 0,
+                        color: "white",
+                        height: "40px",
+                        width: "40px",
+                      }}
+                    />
                   )}
                 </Tooltip>
               </p>
             </div>
           )}
-        </NavbarToggle>
-        <Nav.Link className={classes.link} href="/dashboard">
+        </NavToggle>
+        <LinkItem href="/dashboard">
           {open ? (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"Dashboard"}>
                   <Microsoft />
@@ -156,7 +245,7 @@ export const UserSidebar = () => {
               </p>
             </div>
           ) : (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"Dashboard"}>
                   <Microsoft />
@@ -164,10 +253,10 @@ export const UserSidebar = () => {
               </p>
             </div>
           )}
-        </Nav.Link>
-        <Nav.Link className={classes.link} href="/myteam">
+        </LinkItem>
+        <LinkItem href="/myteam">
           {open ? (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"My Team"}>
                   <Journal />
@@ -175,7 +264,7 @@ export const UserSidebar = () => {
               </p>
             </div>
           ) : (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"My Team"}>
                   <Journal />
@@ -183,10 +272,10 @@ export const UserSidebar = () => {
               </p>
             </div>
           )}
-        </Nav.Link>
-        <Nav.Link className={classes.link} href="/schedule">
+        </LinkItem>
+        <LinkItem href="/schedule">
           {open ? (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"Games"}>
                   <LayoutTextWindow />
@@ -194,7 +283,7 @@ export const UserSidebar = () => {
               </p>
             </div>
           ) : (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"Games"}>
                   <LayoutTextWindow />
@@ -202,10 +291,10 @@ export const UserSidebar = () => {
               </p>
             </div>
           )}
-        </Nav.Link>
-        <Nav.Link className={classes.link} href="/standings">
+        </LinkItem>
+        <LinkItem href="/standings">
           {open ? (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"Stands"}>
                   <Clipboard2DataFill />
@@ -213,7 +302,7 @@ export const UserSidebar = () => {
               </p>
             </div>
           ) : (
-            <div className={classes.menuItem}>
+            <div>
               <p>
                 <Tooltip title={"Stands"}>
                   <Clipboard2DataFill />
@@ -221,9 +310,9 @@ export const UserSidebar = () => {
               </p>
             </div>
           )}
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+        </LinkItem>
+      </Navigation>
+    </Drawer>
   );
 };
 
