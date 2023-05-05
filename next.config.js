17eb232cc07fefcaad50b30b24805f6b35d0c3a1
Jose <bubablue00@gmail.com>
2023-05-05 15:22:24 +0200

    add module federation plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  webpack: (config, options) => {
+    const { buildId, dev, isServer, defaultLoaders, webpack } = options;
+    const mfConf = {
+      name: 'nhlTeams',
+      library: { type: config.output.libraryTarget, name: 'nhlTeams' },
+      filename: 'static/runtime/remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './Sidebar': './components/Sidebar/Sidebar.tsx',
+      },
+      shared: ["react", "react-dom"],
+    };
+    config.plugins.push(new NextFederationPlugin(mfConf, options));
+    if (!isServer) {
+      config.output.publicPath = 'http://localhost:3001/_next/';
+    }
+    return config;
+  }
 }
 
 module.exports = nextConfig
