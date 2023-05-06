897db4b51563b5d0123f6dfcb15d733779461fe0
Jose <bubablue00@gmail.com>
2023-05-06 16:46:38 +0200

    Revert "redo app"
    This reverts commit 4b6c4da2221d94e966ca951bd37de05b1ac7ccca.

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,13 @@
 // import '@/styles/globals.css'
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import TeamsProvider, { useTeams } from "../Context/TeamProvider";
-import NhlThemeProvider from "../Context/Theme/ThemeProvider";
-import Sidebar from "../Components/Sidebar/Sidebar";
-import UserSidebar from "../Components/Sidebar/UserSidebar/UserSidebar";
+// import { QueryClient, QueryClientProvider } from "react-query";
+// import TeamsProvider, { useTeams } from "../Context/TeamProvider";
+// import NhlThemeProvider from "../Context/Theme/ThemeProvider";
+// import Sidebar from "../Components/Sidebar/Sidebar";
+// import UserSidebar from "../Components/Sidebar/UserSidebar/UserSidebar";
 
 export default function App({ Component, pageProps }) {
-  const queryClient = new QueryClient();
-  const {logout} = useTeams()
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TeamsProvider>
-        <NhlThemeProvider>
-          <div>
-            <Sidebar logout={logout} />
-            <Component {...pageProps} />
-            <UserSidebar />
-          </div>
-        </NhlThemeProvider>
-      </TeamsProvider>
-    </QueryClientProvider>
-  );
+  // const queryClient = new QueryClient();
+  // const {logout} = useTeams()
+  return <Component {...pageProps} />;
 }
