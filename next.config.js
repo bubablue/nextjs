f090567f72874ba70de049242f947b3b9f985641
Jose <bubablue00@gmail.com>
2023-05-06 12:44:09 +0200

    app

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         name: "nhlTeams",
         filename: "static/remoteEntry.js",
         exposes: {
-          './App': './pages/_app.js',
+          './App': './pages/_app',
         },
         shared: {},
       })
