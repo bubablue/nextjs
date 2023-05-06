b318dbe1542d85901d25127791b09f328bcec24d
Jose <bubablue00@gmail.com>
2023-05-06 12:33:41 +0200

    update webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-}
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+  webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "nhlTeams",
+        filename: "static/remoteEntry.js",
+        exposes: {
+          './_app': './pages/_app.js',
+        },
+        shared: {},
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
