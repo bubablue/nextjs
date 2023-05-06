e21e6328c39af17dfe66d70a639ac5472f492158
Jose <bubablue00@gmail.com>
2023-05-06 11:57:10 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 // import '@/styles/globals.css'
-import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+// import { QueryClient, QueryClientProvider } from "react-query";
 // import TeamsProvider, { useTeams } from "../Context/TeamProvider";
 // import NhlThemeProvider from "../Context/Theme/ThemeProvider";
 // import Sidebar from "../Components/Sidebar/Sidebar";
 // import UserSidebar from "../Components/Sidebar/UserSidebar/UserSidebar";
 
 export default function App({ Component, pageProps }) {
-  const queryClient = new QueryClient();
+  // const queryClient = new QueryClient();
   // const {logout} = useTeams()
   return (
-    <QueryClientProvider client={queryClient}>
+    // <QueryClientProvider client={queryClient}>
       {/* <TeamsProvider>
         <NhlThemeProvider>
           <div>
@@ -21,6 +20,6 @@ export default function App({ Component, pageProps }) {
         </NhlThemeProvider>
       </TeamsProvider> */}
       <Component {...pageProps} />
-    </QueryClientProvider>
+    // </QueryClientProvider>
   );
 }
