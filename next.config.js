22f068aa8a22d9b95694f5e70d0d117e76a2bcc1
Jose <bubablue00@gmail.com>
2023-05-06 16:47:13 +0200

    Revert "expose: pages"
    This reverts commit ba022853c4d7bd2c358eb798ff7a609a328bddb0.

--- next.config.js
+++ next.config.js
@@ -13,13 +13,6 @@ const nextConfig = {
       exposes: {
         './Sidebar': './Components/Sidebar/Sidebar.tsx',
         './App': './pages/_app.js',
-        './TeamProvider': './Context/TeamProvider.tsx',
-        './Theme/ThemeProvider': './Context/Theme/ThemeProvider.tsx',
-        './UserSidebar': './Components/Sidebar/UserSidebar/UserSidebar.tsx',
-        './schedule': './pages/schedule/index.tsx',
-        './teams': './pages/teams/index.tsx',
-        './team/[id]': './pages/team/[id].tsx',
-        './standings': './pages/standings/index.tsx',
       },
       shared: ["react", "react-dom"],
     };
